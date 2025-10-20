9e768859a0e18a1b7818a52d1a35e60c28ac393e
BT01257 <devdwuti@gmail.com>
2025-10-20 17:02:38 +0530

    form updated

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.static("public"));
 app.use(methodOverride("_method"));
 app.engine('ejs', engine);
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use("/listing",listing);
 app.use("/",userrout);
